5ab4442ffcb074f043e64cda669ef85c5a819e9c
mykarimi <yassine.karimi@gmail.com>
2016-12-09 04:25:15 +0100

    Last updates before the test; the update consists of the new EmotionTagger en the Entity Linking to DBPedia

---
+++ emotionStream.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+
+java -Xmx2000m -cp lib/emotiontagger-v1.0-jar-with-dependencies.jar TagTextStreamWithEmotionsAndExpression --emotion-lexicon "resources/NRC-emotion-lexicon-wordlevel-alphabetized-v0.92.txt" --intensifiers "resources/intensifiers.txt"  --intensifiers "resources/weakeners.txt"
