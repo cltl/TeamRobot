43c9210d4bc09a49e024251c1f7044e5174e5bbb
jasper <jasper@vanderstoop.nl>
2017-01-23 17:01:45 +0100

    Forgot to update the reference to the EmotionTagger resources.

--- emotionStream.sh
+++ emotionStream.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 
-java -Xmx2000m -cp EmotionTagger/release/lib/emotiontagger-v1.0-jar-with-dependencies.jar TagTextStreamWithEmotionsAndExpression --emotion-lexicon "resources/NRC-emotion-lexicon-wordlevel-alphabetized-v0.92.txt" --intensifiers "resources/intensifiers.txt"  --intensifiers "resources/weakeners.txt"
+java -Xmx2000m -cp EmotionTagger/release/lib/emotiontagger-v1.0-jar-with-dependencies.jar TagTextStreamWithEmotionsAndExpression --emotion-lexicon "EmotionTagger/release/resources/NRC-emotion-lexicon-wordlevel-alphabetized-v0.92.txt" --intensifiers "EmotionTagger/release/resources/intensifiers.txt"  --intensifiers "EmotionTagger/release/resources/weakeners.txt"
