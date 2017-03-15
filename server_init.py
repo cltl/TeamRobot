88eff2b23e1f1031fa8cd0408dab398b1a7d26e8
jasper <jasper@vanderstoop.nl>
2017-03-15 03:10:44 +0100

    Remove unneeded files for web client branch

--- server_init.py
+++
@@ -1,7 +0,0 @@
-import sqlite3 as lite
-
-con = lite.connect('server/log.db')
-
-with con:
-    cur = con.cursor()
-    cur.execute("CREATE TABLE Messages(Id INTEGER PRIMARY KEY AUTOINCREMENT , Sender TEXT, Receiver TEXT, Text TEXT, Date DATE)")
