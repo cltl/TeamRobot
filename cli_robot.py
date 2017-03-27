aeef56cc407e764900de2f63fea594fcbc467927
Marieke van Erp <marieke.van.erp@vu.nl>
2017-03-27 16:29:24 +0200

    amended output

--- cli_robot.py
+++ cli_robot.py
@@ -8,10 +8,15 @@ while(True):
     result = pepper.annotate_and_respond(pepper_text, detailed=True)
     print('\n=====')
     print('Concept:\n\t{}\n'
-          'Concept Type:\n\t{}\n'
-          'Emotion:\n\t{}\n'
-          'Topic:\n\t{}'
-          .format(result['concept'], result['concept_type'], result['emotion'], result['topic']))
+ 		  'Emotion:\n\t{}\n'
+          'Topic:\n\t{}'.format(result['concept'], result['emotion'], result['topic']))
     print('Responses:')
     pprint(result['responses'])
     print('=====\n')
+    
+    
+#    print('Concept:\n\t{}\n'
+ #         'Concept Type:\n\t{}\n'
+ #         'Emotion:\n\t{}\n'
+ #         'Topic:\n\t{}'
+ #         .format(result['concept'], result['concept_type'], result['emotion'], result['topic']))    
